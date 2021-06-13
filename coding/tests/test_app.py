916979c40191e1831b1a6bfb659e12da28ed453d
/Vervelak <vervelak@gmail.com>
2021-06-14 00:16:20 +0100

    Add tests for expected and non expected values

--- coding/tests/test_app.py
+++ coding/tests/test_app.py
@@ -20,3 +20,27 @@ def test_multiple_of_seven_and_nine(app, client):
     data = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
     assert data == 'EG'
+
+def test_not_multiple_positive(app, client):
+    del app
+    response = client.get('/api/v1/33')
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data == 33
+
+def test_not_multiple_negative(app, client):
+    del app
+    response = client.get('/api/v1/-33')
+    data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert data == -33
+
+def test_float(app, client):
+    del app
+    response = client.get('/api/v1/0.312')
+    assert response.status_code == 404
+
+def test_str(app, client):
+    del app
+    response = client.get('/api/v1/abc')
+    assert response.status_code == 404
